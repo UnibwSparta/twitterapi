d1f361091b719fa1d1da9714c07c78f2fd8d34e1
sx3eanne <andreas.neumeier@unibw.de>
2023-12-07 13:44:14 +0100

    Add new user fields

--- src/sparta/twitterapi/tweets/constants.py
+++ src/sparta/twitterapi/tweets/constants.py
@@ -4,7 +4,7 @@ FIELD_SELECTORS = "tweet.fields=id,created_at,text,author_id,in_reply_to_user_id
 
 TWEET_FIELDS = "attachments,author_id,context_annotations,conversation_id,created_at,edit_controls,edit_history_tweet_ids,entities,geo,id,in_reply_to_user_id,lang,note_tweet,possibly_sensitive,public_metrics,referenced_tweets,reply_settings,source,text,withheld"  # noqa
 EXPANSIONS = "attachments.media_keys,attachments.poll_ids,author_id,edit_history_tweet_ids,entities.mentions.username,geo.place_id,in_reply_to_user_id,entities.note.mentions.username,referenced_tweets.id,referenced_tweets.id.author_id"  # noqa
-USER_FIELDS = "created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld"
+USER_FIELDS = "created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,subscription_type,url,username,verified,verified_type,withheld"
 MEDIA_FIELDS = "media_key,duration_ms,height,preview_image_url,type,url,width,public_metrics,alt_text,variants"
 POLL_FIELDS = "duration_minutes,end_datetime,id,options,voting_status"
 PLACE_FIELDS = "contained_within,country,country_code,full_name,geo,id,name,place_type"
